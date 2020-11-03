e4ec17bfadf8d0b997baa9618b0ea9461fc37841
Bryan Kim <34020408+bryantaekim@users.noreply.github.com>
2020-11-03 14:52:58 -0500

    Update scorecard.py

--- scorecard.py
+++ scorecard.py
@@ -5,7 +5,7 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('score_card').getOrCreate()
 graph_loc = 'somewhere'
 
-def scorecard(clf, input_tbl, df, target, filename):
+def scoreUnseen(clf, input_tbl, df, target, filename):
     '''
     df : pandas df with WoE
     
@@ -16,35 +16,34 @@ def scorecard(clf, input_tbl, df, target, filename):
     
     '''
     df_feat = df.drop(['party_id', target], axis=1)
-    
     incpt = clf.intercept_
     model_params = pd.DataFrame(clf.coef_.ravel(), columns=['coefficient'], index=df_feat.columns)
         
-    scorecard = df_feat[model_params.index].apply(lambda x: x*model_params['coefficient'].T, axis=1)
-    scorecard[target] = df[target]
-    scorecard['logit'] = scorecard[scorecard.columns].sum(axis=1) + incpt
-    scorecard['odds'] = np.exp(scorecard['logit'])
-    scorecard['probs'] = scorecard.odds / (1 + scorecard.odds)
+    scorecard = df[model_params.index].apply(lambda x: x*model_params['coefficient'].T, axis=1)
+    scorecard.loc[:, 'party_id'] = df[['party_id']]
+    scorecard.loc[:, target] = df[target]
+    scorecard.loc[:, 'pred_prob_0'] = clf.predict_proba(df_feat)[:,0]
+    scorecard.loc[:, 'pred_prob_1'] = clf.predict_proba(df_feat)[:,1]
+    scorecard.loc[:, 'logit'] = scorecard[scorecard.columns.difference(['party_id'])].sum(axis=1) + incpt
+    scorecard.loc[:, 'odds'] = np.exp(scorecard['logit'])
+    scorecard.loc[:, 'probs'] = scorecard.odds / (1 + scorecard.odds)
     
     # Scoring baseline
     target_score = 600
     pdo = 40
-    _for = len(df[df[target] == 1])
-    _against = len(df[df[target] == 0])
+    _for = len(scorecard[scorecard['pred_prob_1'] >= .5])
+    _against = len(scorecard[scorecard['pred_prob_1'] < .5])
     target_odds = float(_for / _against)
-    
     factor =  pdo / np.log(2)
 
-    scorecard['score'] = target_score - (factor * np.log(target_odds)) + factor * scorecard['logit']
-    scorecard['pred_prob_0'] = clf.predict_proba(df_feat)[:,0]
-    scorecard['pred_prob_1'] = clf.predict_proba(df_feat)[:,1]
-    scorecard['decile'] = pd.qcut(scorecard['pred_prob_1'].rank(method = 'first'), 10, labels = range(10,0,-1)).astype(float)
+    scorecard.loc[:, 'score'] = target_score - (factor * np.log(target_odds)) + factor * scorecard['logit']
+    scorecard.loc[:, 'decile'] = pd.qcut(scorecard['pred_prob_1'].rank(method = 'first'), 10, labels = range(10,0,-1)).astype(float)
     
     print(scorecard.head())
     
     scores = spark.createDataFrame(scorecard)
     scores.write.mode('overwrite').saveAsTable(input_tbl + '_prediction_' + target)
-    scores.groupby(target, 'decile').count().show()
+    scores.groupby(target, 'decile').count().orderBy(target, 'decile').show()
     
     # Plot Distribution of Scores
     plt.figure(figsize=(12,8))
